09863dec17c0cdc7dc2fc21c72a524557182a509
ZXY-CC-3ag13 <ZXY-CC-3ag13@protonmail.com>
2023-06-12 19:16:35 +0545

    Rating function added

--- Bibek.session.sql
+++ Bibek.session.sql
@@ -1,340 +1,645 @@
 -- phpMyAdmin SQL Dump
+
 -- version 5.1.3
+
 -- https://www.phpmyadmin.net/
+
 --
+
 -- Host: 127.0.0.1
+
 -- Generation Time: Jun 12, 2023 at 10:46 AM
+
 -- Server version: 10.4.22-MariaDB
+
 -- PHP Version: 7.4.28
 
 DROP DATABASE IF EXISTS `maher`;
-CREATE DATABASE IF NOT EXISTS `maher` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+
+CREATE DATABASE
+    IF NOT EXISTS `maher` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+
 USE `maher`;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+
 START TRANSACTION;
+
 SET time_zone = "+00:00";
 
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+;
+
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */
+
+;
+
+/*!40101 SET NAMES utf8mb4 */
+
+;
 
 --
+
 -- Database: `maher`
+
 --
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `addcart`
---
-
-CREATE TABLE `addcart` (
-  `id` bigint(12) NOT NULL,
-  `p_id` bigint(12) NOT NULL,
-  `u_id` varchar(50) NOT NULL,
-  `price` bigint(12) NOT NULL,
-  `qty` bigint(12) NOT NULL,
-  `total` bigint(12) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `addcart` (
+        `id` bigint(12) NOT NULL,
+        `p_id` bigint(12) NOT NULL,
+        `u_id` varchar(50) NOT NULL,
+        `price` bigint(12) NOT NULL,
+        `qty` bigint(12) NOT NULL,
+        `total` bigint(12) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `addcart`
+
 --
 
-INSERT INTO `addcart` (`id`, `p_id`, `u_id`, `price`, `qty`, `total`) VALUES
-(22, 40, 'abcd', 200, 1, 200);
+INSERT INTO
+    `addcart` (
+        `id`,
+        `p_id`,
+        `u_id`,
+        `price`,
+        `qty`,
+        `total`
+    )
+VALUES (22, 40, 'abcd', 200, 1, 200);
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `admin`
---
-
-CREATE TABLE `admin` (
-  `adminid` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `admin` (
+        `adminid` varchar(50) NOT NULL,
+        `password` varchar(50) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `admin`
+
 --
 
-INSERT INTO `admin` (`adminid`, `password`) VALUES
-('admin', 'admin');
+INSERT INTO
+    `admin` (`adminid`, `password`)
+VALUES ('admin', 'admin');
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `checkout`
---
-
-CREATE TABLE `checkout` (
-  `id` bigint(12) NOT NULL,
-  `p_id` bigint(12) NOT NULL,
-  `u_id` varchar(50) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `mobile` bigint(12) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `location` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `checkout` (
+        `id` bigint(12) NOT NULL,
+        `p_id` bigint(12) NOT NULL,
+        `u_id` varchar(50) NOT NULL,
+        `name` varchar(100) NOT NULL,
+        `mobile` bigint(12) NOT NULL,
+        `email` varchar(150) NOT NULL,
+        `location` text NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `checkout`
+
 --
 
-INSERT INTO `checkout` (`id`, `p_id`, `u_id`, `name`, `mobile`, `email`, `location`) VALUES
-(88, 38, 'abcd', 'samir', 9818453551, 'samirdotel1234@gmail.com', 'Balaju'),
-(89, 40, 'abcd', 'samir', 9818453551, 'samirdotel1234@gmail.com', 'Balaju');
+INSERT INTO
+    `checkout` (
+        `id`,
+        `p_id`,
+        `u_id`,
+        `name`,
+        `mobile`,
+        `email`,
+        `location`
+    )
+VALUES (
+        88,
+        38,
+        'abcd',
+        'samir',
+        9818453551,
+        'samirdotel1234@gmail.com',
+        'Balaju'
+    ), (
+        89,
+        40,
+        'abcd',
+        'samir',
+        9818453551,
+        'samirdotel1234@gmail.com',
+        'Balaju'
+    );
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `food_cat`
---
-
-CREATE TABLE `food_cat` (
-  `id` bigint(12) NOT NULL,
-  `catnm` varchar(100) NOT NULL,
-  `sub_cat` varchar(120) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `food_cat` (
+        `id` bigint(12) NOT NULL,
+        `catnm` varchar(100) NOT NULL,
+        `sub_cat` varchar(120) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `food_cat`
+
 --
 
-INSERT INTO `food_cat` (`id`, `catnm`, `sub_cat`) VALUES
-(72, 'kathiyawadi', 'Chicken Momo'),
-(73, 'kathiyawadi', 'Veg Momo'),
-(74, 'kathiyawadi', 'Buff Momo'),
-(75, 'kathiyawadi', 'Fry Chicken Momo'),
-(76, 'kathiyawadi', 'Fry Chicken C Momo'),
-(77, 'kathiyawadi', 'Fry Buff Momo'),
-(78, 'rajsthani', 'Chicken Chowmein'),
-(83, 'rajsthani', 'Chop suey'),
-(84, 'rajsthani', 'Thupka'),
-(85, 'rajsthani', 'Buff Chowmein'),
-(86, 'rajsthani', 'Veg Chowmein'),
-(87, 'rise', 'Dal Bhat'),
-(88, 'rise', 'Chicken Dal Bhat'),
-(89, 'tava', 'Chicken Burger'),
-(90, 'tava', 'Buff Burger'),
-(91, 'kathiyawadi', 'Veg Momo');
+INSERT INTO
+    `food_cat` (`id`, `catnm`, `sub_cat`)
+VALUES (
+        72,
+        'kathiyawadi',
+        'Chicken Momo'
+    ), (73, 'kathiyawadi', 'Veg Momo'), (74, 'kathiyawadi', 'Buff Momo'), (
+        75,
+        'kathiyawadi',
+        'Fry Chicken Momo'
+    ), (
+        76,
+        'kathiyawadi',
+        'Fry Chicken C Momo'
+    ), (
+        77,
+        'kathiyawadi',
+        'Fry Buff Momo'
+    ), (
+        78,
+        'rajsthani',
+        'Chicken Chowmein'
+    ), (83, 'rajsthani', 'Chop suey'), (84, 'rajsthani', 'Thupka'), (
+        85,
+        'rajsthani',
+        'Buff Chowmein'
+    ), (
+        86,
+        'rajsthani',
+        'Veg Chowmein'
+    ), (87, 'rise', 'Dal Bhat'), (88, 'rise', 'Chicken Dal Bhat'), (89, 'tava', 'Chicken Burger'), (90, 'tava', 'Buff Burger'), (91, 'kathiyawadi', 'Veg Momo');
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `food_parcel`
+
 --
 
-CREATE TABLE `food_parcel` (
-  `id` bigint(12) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `mobile` bigint(12) NOT NULL,
-  `email` varchar(150) NOT NULL,
-  `address` text NOT NULL,
-  `food_id` bigint(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+CREATE TABLE
+    `food_parcel` (
+        `id` bigint(12) NOT NULL,
+        `name` varchar(100) NOT NULL,
+        `mobile` bigint(12) NOT NULL,
+        `email` varchar(150) NOT NULL,
+        `address` text NOT NULL,
+        `food_id` bigint(10) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `gallery`
---
-
-CREATE TABLE `gallery` (
-  `id` bigint(12) NOT NULL,
-  `image` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `gallery` (
+        `id` bigint(12) NOT NULL,
+        `image` text NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `gallery`
+
 --
 
-INSERT INTO `gallery` (`id`, `image`) VALUES
-(8, 'img/thali.jpg'),
-(9, 'img/chines.jpg'),
-(10, 'img/abc.jpg');
+INSERT INTO
+    `gallery` (`id`, `image`)
+VALUES (8, 'img/thali.jpg'), (9, 'img/chines.jpg'), (10, 'img/abc.jpg');
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `menu`
---
-
-CREATE TABLE `menu` (
-  `id` bigint(12) NOT NULL,
-  `category` varchar(100) NOT NULL,
-  `sub_cat` varchar(120) NOT NULL,
-  `title` varchar(100) NOT NULL,
-  `description` text NOT NULL,
-  `price` bigint(6) NOT NULL,
-  `image` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `menu` (
+        `id` bigint(12) NOT NULL,
+        `category` varchar(100) NOT NULL,
+        `sub_cat` varchar(120) NOT NULL,
+        `title` varchar(100) NOT NULL,
+        `description` text NOT NULL,
+        `price` bigint(6) NOT NULL,
+        `image` text NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `menu`
+
 --
 
-INSERT INTO `menu` (`id`, `category`, `sub_cat`, `title`, `description`, `price`, `image`) VALUES
-(35, 'Momo', 'Chicken Momo', 'Chicken Momo', 'Juicy Chicken Momo', 150, 'mimg/chicken-momo.jpg'),
-(36, 'Momo', 'Veg Momo', 'Veg Momo', 'Vegetable Momo', 100, 'mimg/veg-momo.png'),
-(37, 'Momo', 'Buff Momo', 'Buff Momo', 'Buff Momo', 125, 'mimg/buffmomo.jpg'),
-(38, 'Chowmein', 'Chicken Chowmein', 'Chicken Chowmein', 'Chicken Chowmein', 150, 'mimg/chickenchowmein.jpg'),
-(39, 'Chowmein', 'Thupka', 'Thupka', 'Thupka', 150, 'mimg/ThukpaI.jpg'),
-(40, 'Dal Bhat', 'Dal Bhat', 'Sada Dal bhat', 'Sada Dal Bhat', 200, 'mimg/DalBhat.jpg'),
-(41, 'Dal Bhat', 'Chicken Dal Bhat', 'Chicken Dal Bhat', 'Chicken Dal Bhat', 250, 'mimg/ChickenDalbhat.jpg'),
-(42, 'Burger', 'Chicken Burger', 'Chicken Burger', 'Chicken Burger', 250, 'mimg/Chicken-Burgers-5.jpg'),
-(43, 'Burger', 'Buff Burger', 'Buff Burger', 'Buff Burger', 220, 'mimg/Buffberger.jpg'),
-(44, 'rise', 'Chicken Dal Bhat', 'Dal Bhat special', 'one of the Kind of special nepalese dal bhat', 300, 'mimg/blog-img-02.jpg'),
-(45, 'rise', 'Chicken Dal Bhat', 'aalu', 'aalu', 12, 'mimg/avt-img.jpg');
+INSERT INTO
+    `menu` (
+        `id`,
+        `category`,
+        `sub_cat`,
+        `title`,
+        `description`,
+        `price`,
+        `image`
+    )
+VALUES (
+        35,
+        'Momo',
+        'Chicken Momo',
+        'Chicken Momo',
+        'Juicy Chicken Momo',
+        150,
+        'mimg/chicken-momo.jpg'
+    ), (
+        36,
+        'Momo',
+        'Veg Momo',
+        'Veg Momo',
+        'Vegetable Momo',
+        100,
+        'mimg/veg-momo.png'
+    ), (
+        37,
+        'Momo',
+        'Buff Momo',
+        'Buff Momo',
+        'Buff Momo',
+        125,
+        'mimg/buffmomo.jpg'
+    ), (
+        38,
+        'Chowmein',
+        'Chicken Chowmein',
+        'Chicken Chowmein',
+        'Chicken Chowmein',
+        150,
+        'mimg/chickenchowmein.jpg'
+    ), (
+        39,
+        'Chowmein',
+        'Thupka',
+        'Thupka',
+        'Thupka',
+        150,
+        'mimg/ThukpaI.jpg'
+    ), (
+        40,
+        'Dal Bhat',
+        'Dal Bhat',
+        'Sada Dal bhat',
+        'Sada Dal Bhat',
+        200,
+        'mimg/DalBhat.jpg'
+    ), (
+        41,
+        'Dal Bhat',
+        'Chicken Dal Bhat',
+        'Chicken Dal Bhat',
+        'Chicken Dal Bhat',
+        250,
+        'mimg/ChickenDalbhat.jpg'
+    ), (
+        42,
+        'Burger',
+        'Chicken Burger',
+        'Chicken Burger',
+        'Chicken Burger',
+        250,
+        'mimg/Chicken-Burgers-5.jpg'
+    ), (
+        43,
+        'Burger',
+        'Buff Burger',
+        'Buff Burger',
+        'Buff Burger',
+        220,
+        'mimg/Buffberger.jpg'
+    ), (
+        44,
+        'rise',
+        'Chicken Dal Bhat',
+        'Dal Bhat special',
+        'one of the Kind of special nepalese dal bhat',
+        300,
+        'mimg/blog-img-02.jpg'
+    ), (
+        45,
+        'rise',
+        'Chicken Dal Bhat',
+        'aalu',
+        'aalu',
+        12,
+        'mimg/avt-img.jpg'
+    );
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `registration`
---
-
-CREATE TABLE `registration` (
-  `id` bigint(12) NOT NULL,
-  `userid` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL,
-  `email` varchar(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
+
+CREATE TABLE
+    `registration` (
+        `id` bigint(12) NOT NULL,
+        `userid` varchar(50) NOT NULL,
+        `password` varchar(50) NOT NULL,
+        `email` varchar(150) NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
 -- Dumping data for table `registration`
+
 --
 
-INSERT INTO `registration` (`id`, `userid`, `password`, `email`) VALUES
-(1, 'abcd', 'xyz', 'abc@yahoo.com'),
-(2, 'aa', 'bb', 'cc@y.com'),
-(3, 'nilkanth', 'nilkanth', 'aaabb@y.com'),
-(4, 'abcd', 'ppp', 'abc@yahoo.com'),
-(5, 'abcd', 'asdf', 'abc@yahoo.com');
+INSERT INTO
+    `registration` (
+        `id`,
+        `userid`,
+        `password`,
+        `email`
+    )
+VALUES (
+        1,
+        'abcd',
+        'xyz',
+        'abc@yahoo.com'
+    ), (2, 'aa', 'bb', 'cc@y.com'), (
+        3,
+        'nilkanth',
+        'nilkanth',
+        'aaabb@y.com'
+    ), (
+        4,
+        'abcd',
+        'ppp',
+        'abc@yahoo.com'
+    ), (
+        5,
+        'abcd',
+        'asdf',
+        'abc@yahoo.com'
+    );
 
 -- --------------------------------------------------------
 
 --
+
 -- Table structure for table `review`
+
 --
 
-CREATE TABLE `review` (
-  `id` bigint(12) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `review` varchar(100) NOT NULL,
-  `description` text NOT NULL
+CREATE TABLE
+    `review` (
+        `id` bigint(12) NOT NULL,
+        `name` varchar(100) NOT NULL,
+        `review` varchar(100) NOT NULL,
+        `description` text NOT NULL
+    ) ENGINE = InnoDB DEFAULT CHARSET = latin1;
+
+--
+
+-- Dumping data for table `review`
+
+--
+
+INSERT INTO
+    `review` (
+        `id`,
+        `name`,
+        `review`,
+        `description`
+    )
+VALUES (
+        7,
+        'Samir',
+        'Exellent',
+        'Nice restaurant'
+    ), (8, 'Ram', 'Good', 'Nice Place'), (
+        9,
+        'Hari',
+        'Exellent',
+        'Good Foods'
+    );
+
+CREATE TABLE ratings (
+    id bigint(12) NOT NULL AUTO_INCREMENT,
+    userid varchar(50) NOT NULL,
+    pid BIGINT NOT NULL,
+    rating INT NOT NULL,
+    PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `review`
---
 
-INSERT INTO `review` (`id`, `name`, `review`, `description`) VALUES
-(7, 'Samir', 'Exellent', 'Nice restaurant'),
-(8, 'Ram', 'Good', 'Nice Place'),
-(9, 'Hari', 'Exellent', 'Good Foods');
-
---
 -- Indexes for dumped tables
+
 --
 
 --
+
 -- Indexes for table `addcart`
---
-ALTER TABLE `addcart`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `addcart` ADD PRIMARY KEY (`id`);
+
+--
+
 -- Indexes for table `admin`
---
-ALTER TABLE `admin`
-  ADD PRIMARY KEY (`adminid`);
 
 --
+
+ALTER TABLE `admin` ADD PRIMARY KEY (`adminid`);
+
+--
+
 -- Indexes for table `checkout`
---
-ALTER TABLE `checkout`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `checkout` ADD PRIMARY KEY (`id`);
+
+--
+
 -- Indexes for table `food_cat`
---
-ALTER TABLE `food_cat`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `food_cat` ADD PRIMARY KEY (`id`);
+
+--
+
 -- Indexes for table `gallery`
---
-ALTER TABLE `gallery`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `gallery` ADD PRIMARY KEY (`id`);
+
+--
+
 -- Indexes for table `menu`
---
-ALTER TABLE `menu`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `menu` ADD PRIMARY KEY (`id`);
+
+--
+
 -- Indexes for table `registration`
---
-ALTER TABLE `registration`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `registration` ADD PRIMARY KEY (`id`);
+
+--
+
 -- Indexes for table `review`
---
-ALTER TABLE `review`
-  ADD PRIMARY KEY (`id`);
 
 --
+
+ALTER TABLE `review` ADD PRIMARY KEY (`id`);
+
+--
+
 -- AUTO_INCREMENT for dumped tables
+
 --
 
 --
+
 -- AUTO_INCREMENT for table `addcart`
---
-ALTER TABLE `addcart`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
+
+ALTER TABLE
+    `addcart` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 23;
+
+--
+
 -- AUTO_INCREMENT for table `checkout`
---
-ALTER TABLE `checkout`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=90;
 
 --
+
+ALTER TABLE
+    `checkout` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 90;
+
+--
+
 -- AUTO_INCREMENT for table `food_cat`
---
-ALTER TABLE `food_cat`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=92;
 
 --
+
+ALTER TABLE
+    `food_cat` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 92;
+
+--
+
 -- AUTO_INCREMENT for table `gallery`
---
-ALTER TABLE `gallery`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
+
+ALTER TABLE
+    `gallery` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 11;
+
+--
+
 -- AUTO_INCREMENT for table `menu`
---
-ALTER TABLE `menu`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=46;
 
 --
+
+ALTER TABLE
+    `menu` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 46;
+
+--
+
 -- AUTO_INCREMENT for table `registration`
---
-ALTER TABLE `registration`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
--- AUTO_INCREMENT for table `review`
+
+ALTER TABLE
+    `registration` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 6;
+
 --
-ALTER TABLE `review`
-  MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+-- AUTO_INCREMENT for table `review`
+
+--
+
+ALTER TABLE
+    `review` MODIFY `id` bigint(12) NOT NULL AUTO_INCREMENT,
+    AUTO_INCREMENT = 10;
+
+-- RATING TABLE
+
+-- Userid, productid, rating
+
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */
+
+;
+
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */
+
+;
+
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */
+
+;
--- rate.php
+++ rate.php
@@ -1,13 +1,95 @@
 <?php session_start(); ?>
 <?php include "header.php"; ?>
+<style>
+    .rating {
+        font-size: 24px;
+        color: #FFD700;
+        /* Yellow color for stars */
+    }
+
+    div.stars {
+
+        width: 270px;
+
+        display: inline-block;
+
+    }
+
+    .mt-200 {
+        margin-top: 200px;
+    }
+
+    input.star {
+        display: none;
+    }
 
 
-<div class="container ">
+
+    label.star {
+
+        float: right;
+
+        padding: 10px;
+        margin-left: 0.9em;
+
+        font-size: 15px;
+
+        color: #4A148C;
+
+        transition: all .2s;
+
+    }
+
+
+
+    input.star:checked~label.star:before {
+
+        content: '\f005';
+
+        color: #FD4;
+
+        transition: all .25s;
+
+    }
+
+
+    input.star-5:checked~label.star:before {
+
+        color: #FE7;
+
+        text-shadow: 0 0 20px #952;
+
+    }
+
+
+
+    input.star-1:checked~label.star:before {
+        color: #F62;
+    }
+
+
+
+    label.star:hover {
+        transform: rotate(-15deg) scale(1.3);
+    }
+
+
+
+    label.star:before {
+
+        content: '\f006';
+
+        font-family: FontAwesome;
+
+    }
+</style>
+
+<div class="container pt-5">
     <div class="row pt-5">
-        <div class="col-lg-12">
+        <div class="col-lg-12 pt-5">
             <div class="heading-title text-center">
-                <h2>Special Menu</h2>
-                <p>Enjoy the new Tasty</p>
+                <h2>Rate</h2>
+                <p>Rate Past Orders</p>
             </div>
         </div>
     </div>
@@ -23,37 +105,45 @@
                     <?php include "connect.php";
                     $s = mysqli_query($con, "select * from menu as m join checkout as c on m.id = c.p_id");
                     while ($r = mysqli_fetch_array($s)) {
-                        ?>
+                        $select_query = "SELECT * FROM ratings WHERE userid = '" . $_SESSION['uid'] . "'";
+                        $result = mysqli_query($con, $select_query);
+                        $fetch_rating = mysqli_fetch_assoc($result);
+                    ?>
                         <div class="col-lg-4 col-md-6 special-grid">
                             <div class="gallery-single fix">
-                                <img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image"
-                                    style='width: 263px; height:170px;'>
-                                <div class="why-text">
-                                    <h4>
-                                        <?php echo $r['title']; ?>
-                                    </h4>
-                                    <p>
-                                        <?php echo $r['description']; ?>
-                                    </p>
-                                    <p>Rs
-                                        <?php echo $r['price']; ?> /-
-                                    </p>
-                                    <h5 align="center">
-                                        <?php if (isset($_SESSION['uid'])) {
-                                            ?>
-                                            <a
-                                                href="addcart.php?pid=<?php echo $r['id']; ?>&uid=<?php echo $_SESSION['uid']; ?>&price=<?php echo $r['price']; ?>">Add
-                                                Cart</a>
-                                        </h5>
-
-                                        <?php
-                                        } else {
-                                            ?>
-                                        <a href="login.php">Add Cart</a>
-                                    <?php } ?>
-
-                                </div>
+                                <img src="admin/<?php echo $r['image']; ?>" class="img-fluid" alt="Image" style='width: 263px; height:170px;'>
                             </div>
+                            <div class="name">
+                                <?php echo $r['title']; ?>
+                            </div>
+                            <!-- Rating stars -->
+                            <form action="" method="post" class="box">
+                                <input type="hidden" name="item_id" value="<?php echo $r['id']; ?>">
+                                <input class="star star-5" id="star-5-<?php echo $fetch_products['id']; ?>" type="radio" name="star" value="5" <?php if ($fetch_rating && $fetch_rating['rating'] == 5) { echo 'checked';} ?> />
+                                <label class="star star-5" for="star-5-<?php echo $fetch_products['id']; ?>"></label>
+
+                                <input class="star star-4" id="star-4-<?php echo $fetch_products['id']; ?>" type="radio" name="star" value="4" <?php if ($fetch_rating && $fetch_rating['rating'] == 4) {
+                                                                                                                                                    echo 'checked';
+                                                                                                                                                } ?> />
+                                <label class="star star-4" for="star-4-<?php echo $fetch_products['id']; ?>"></label>
+
+                                <input class="star star-3" id="star-3-<?php echo $fetch_products['id']; ?>" type="radio" name="star" value="3" <?php if ($fetch_rating && $fetch_rating['rating'] == 3) {
+                                                                                                                                                    echo 'checked';
+                                                                                                                                                } ?> />
+                                <label class="star star-3" for="star-3-<?php echo $fetch_products['id']; ?>"></label>
+
+                                <input class="star star-2" id="star-2-<?php echo $fetch_products['id']; ?>" type="radio" name="star" value="2" <?php if ($fetch_rating && $fetch_rating['rating'] == 2) {
+                                                                                                                                                    echo 'checked';
+                                                                                                                                                } ?> />
+                                <label class="star star-2" for="star-2-<?php echo $fetch_products['id']; ?>"></label>
+
+                                <input class="star star-1" id="star-1-<?php echo $fetch_products['id']; ?>" type="radio" name="star" value="1" <?php if ($fetch_rating && $fetch_rating['rating'] == 1) {
+                                                                                                                                                    echo 'checked';
+                                                                                                                                                } ?> />
+                                <label class="star star-1" for="star-1-<?php echo $fetch_products['id']; ?>"></label>
+
+                                <input type="submit" value="Rate" name="rate" class="btn">
+                            </form>
                         </div>
                     <?php } ?>
                 </div>
